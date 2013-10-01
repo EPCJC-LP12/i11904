d6683d073a1148c9d5436eaa5c60ef3d4f9b8732
i11904 <i11904@epcjc.net>
2013-10-01 12:41:36 +0100

    acabado o ex1

--- revisao/ex1/ex1/Form1.vb
+++ revisao/ex1/ex1/Form1.vb
@@ -1,3 +1,26 @@
 ﻿Public Class Form1
+    Dim conViagem As Double
+    Dim custViagem As Double
 
+    Private Sub calcular()
+        Dim preco As Double = CDbl(TextBox1.Text)
+        Dim consumo As Double = CDbl(TextBox2.Text)
+        Dim distancia As Double = CDbl(TextBox3.Text)
+
+        conViagem = (consumo * distancia) / 100
+        custViagem = preco * conViagem
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        calcular()
+        Me.TextBox4.Text = conViagem
+        Me.TextBox5.Text = custViagem
+
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
 End Class
