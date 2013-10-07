d720b211060751909c4fec6a31c6c071e06a6a80
i11904 <i11904@epcjc.net>
2013-10-07 12:48:57 +0100

    fim ex2

--- revisao/ex2/ex2/Form1.Designer.vb
+++ revisao/ex2/ex2/Form1.Designer.vb
@@ -22,133 +22,122 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(13, 41)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(52, 20)
-        Me.TextBox1.TabIndex = 0
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(109, 41)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(58, 20)
-        Me.TextBox2.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(173, 44)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(13, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "="
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(205, 41)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.ReadOnly = True
-        Me.TextBox3.Size = New System.Drawing.Size(56, 20)
-        Me.TextBox3.TabIndex = 3
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(13, 106)
+        Me.Button1.Location = New System.Drawing.Point(16, 133)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(39, 23)
-        Me.Button1.TabIndex = 4
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
         Me.Button1.Text = "+"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(79, 106)
+        Me.Button2.Location = New System.Drawing.Point(100, 133)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(38, 23)
-        Me.Button2.TabIndex = 5
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
         Me.Button2.Text = "-"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(136, 106)
+        Me.Button3.Location = New System.Drawing.Point(190, 133)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(38, 23)
-        Me.Button3.TabIndex = 6
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
         Me.Button3.Text = "*"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(205, 106)
+        Me.Button4.Location = New System.Drawing.Point(287, 133)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(34, 23)
-        Me.Button4.TabIndex = 7
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
         Me.Button4.Text = "/"
         Me.Button4.UseVisualStyleBackColor = True
         '
-        'Button5
+        'Label1
         '
-        Me.Button5.Location = New System.Drawing.Point(163, 159)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 23)
-        Me.Button5.TabIndex = 8
-        Me.Button5.Text = "Sair"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(261, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "="
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(64, 44)
+        Me.Label2.Location = New System.Drawing.Point(108, 34)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 9
-        Me.Label2.Text = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 5
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(2, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(155, 31)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(306, 34)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 8
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 194)
+        Me.ClientSize = New System.Drawing.Size(425, 187)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
-        Me.Text = "Operação Ariteméticas"
+        Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Button4 As System.Windows.Forms.Button
-    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
-
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
 End Class
--- revisao/ex2/ex2/Form1.vb
+++ revisao/ex2/ex2/Form1.vb
@@ -1,36 +1,57 @@
 ﻿Public Class Form1
+    Dim n1 As Double
+    Dim n2 As Double
 
-    Dim n1 As Double = CDbl(TextBox1.Text)
-    Dim n2 As Double = CDbl(TextBox2.Text)
+    Private Function soma() As Double
+        Dim s As Double = n1 + n2
+        Return s
+    End Function
 
-    Private Sub soma()
-        Dim soma As Double = n1 + n2
-    End Sub
-    Private Sub subt()
-        Dim subt As Double = n1 - n2
-    End Sub
-    Private Sub mult()
-        Dim mult As Double = n1 * n2
-    End Sub
-    Private Sub div()
-        Dim div As Double = n1 / n2
-    End Sub
+    Private Function subt() As Double
+        Dim subtracao As Double = n1 - n2
+        Return subtracao
+    End Function
+    Private Function mult() As Double
+        Dim multi As Double = n1 * n2
+        Return multi
+    End Function
+
+    Private Function div() As Double
+        Dim divi As Double = n1 / n2
+        Return divi
+    End Function
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Me.TextBox3.Text = n1 + n2
+        n1 = CDbl(TextBox1.Text)
+        n2 = CDbl(TextBox2.Text)
+        Me.Label2.Text = "+"
+        Me.TextBox3.Text = soma()
     End Sub
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        Me.TextBox3.Text = n1 - n2
+        n1 = CDbl(TextBox1.Text)
+        n2 = CDbl(TextBox2.Text)
+        Me.Label2.Text = "-"
+        Me.TextBox3.Text = subt()
     End Sub
 
 
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
-        Me.TextBox3.Text = n1 * n2
+        n1 = CDbl(TextBox1.Text)
+        n2 = CDbl(TextBox2.Text)
+        Me.Label2.Text = "*"
+        Me.TextBox3.Text = mult()
     End Sub
 
 
     Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
-        Me.TextBox3.Text = n1 / n2
+        n1 = CDbl(TextBox1.Text)
+        n2 = CDbl(TextBox2.Text)
+        Me.Label2.Text = "/"
+        Me.TextBox3.Text = div()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        
     End Sub
 End Class
